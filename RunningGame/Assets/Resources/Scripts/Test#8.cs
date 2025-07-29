c526ee8a0cd9d9fa11124510b52a3130181fc69b
zjaxjrhkd <zjaxjrhkd100@gmail.com>
2025-07-29 11:34:25 +0900

    [feat] 한글 주석 테스트 #8

---
+++ RunningGame/Assets/Resources/Scripts/Test#8.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test8 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //한글주석 테스트 박진우
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
